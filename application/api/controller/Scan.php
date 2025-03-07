2100cea13425c9964b25de9bea685cbc180f36fc
kyle <kyle@email.com>
2025-03-07 17:40:54 +0800

    u

---
+++ application/api/controller/Scan.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\api\controller;
+
+use think\Controller;
+
+
+class Scan extends Controller
+{
+    public function check()
+    {
+        echo 1;
+        exit;
+    }
+
+    public function check_translate_field()
+    {
+        echo 1;exit;
+    }
